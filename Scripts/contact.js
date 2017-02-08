44c947f7f10038634c89e0e341f9e90e8d98988f
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-02-08 12:48:01 -0500

    First Commit
    Just copied files from last assignment. Will be making changes

---
+++ Scripts/contact.js
@@ -0,0 +1,14 @@
+/*Author Name= Rajvinder Singh Yogi
+Student Number=200353207
+Website= Mini-portfolio
+File Name= contact.js
+This file includes all the styling properties which makes this website attractive and Improve the design
+
+-----------------------------------------------------------------------------------------------------------*/
+
+// JAVA Script Here*
+
+// Contact page heading
+var ContactMe = document.getElementById("ContactMe");
+var myHeading="Contact Me";
+ContactMe.textContent=myHeading;
---
+++ Scripts/portfolio.js
@@ -0,0 +1,55 @@
+/*Author Name= Rajvinder Singh Yogi
+Student Number=200353207
+Website= Mini-portfolio
+File Name= portfolio.js
+This file includes all the styling properties which makes this website attractive and Improve the design
+
+-----------------------------------------------------------------------------------------------------------*/
+
+// JAVA Script Here
+
+//project1 portfolio content starts
+var headingProject1 = document.getElementById("headingProject1");
+var ContentPortfolio="Live Website";
+headingProject1.textContent=ContentPortfolio;
+
+var subheadingProject1 = document.getElementById("subheadingProject1");
+var ContentPortfolio="Broadway Animal Hospital";
+subheadingProject1.textContent=ContentPortfolio;
+
+var project1Para = document.getElementById("project1Para");
+var ContentPortfolio="I designed this website in wordpress. Owner(my uncle) of website is a veterinarian and owns an animal clinic in Orangeville. This website has a service to book appointments online. This website helped Dr. Yogi to improve their business in many ways.";
+project1Para.textContent=ContentPortfolio;
+//project1 portfolio content ends
+
+// -----------------------------------------------------------------------------------------------------------
+
+//project2 portfolio content starts
+var headingProject2 = document.getElementById("headingProject2");
+var ContentPortfolio="Movie Poster";
+headingProject2.textContent=ContentPortfolio;
+
+var subheadingProject2 = document.getElementById("subheadingProject2");
+var ContentPortfolio="Assignmet 1st Semester";
+subheadingProject2.textContent=ContentPortfolio;
+
+var project2Para = document.getElementById("project2Para");
+var ContentPortfolio="This movie-poster was designed by me in previous semester using CSS and HTML. 'Scott Mcrindlle' guided me in this project. It has the link to original website of 'The Fast and Furious' movie. It helped me alot to learn new properties of CSS. ";
+project2Para.textContent=ContentPortfolio;
+//project2 portfolio content ends
+
+// ----------------------------------------------------------------------------------------------------------------
+
+//project3 portfolio content starts
+var headingProject3 = document.getElementById("headingProject3");
+var ContentPortfolio="Photography";
+headingProject3.textContent=ContentPortfolio;
+
+var subheadingProject3 = document.getElementById("subheadingProject3");
+var ContentPortfolio="Exceptional Photography";
+subheadingProject3.textContent=ContentPortfolio;
+
+var project3Para = document.getElementById("project3Para");
+var ContentPortfolio="I took this picture nearby Bell fountain. This picture is one of the finest Photography done by by me. It was a raining when I took this picture. This picture got reward from some professional photogrphers too. ";
+project3Para.textContent=ContentPortfolio;
+//project3 portfolio content ends
